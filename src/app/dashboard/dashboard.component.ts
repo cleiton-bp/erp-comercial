2a7a8caaf489a0c1d948314e5582f9be4d51a9ac
cleiton-bp <cleitontcpereira@gmail.com>
2025-04-06 20:49:18 -0300

    a

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,16 +1,5 @@
 import { Component } from '@angular/core';
 
-type ImagemPosicao = 'left' | 'right';
-
-interface BlocoTextoImagem {
-  imagem: string;
-  imagemAlt: string;
-  titulo: string;
-  subtitulo: string;
-  descricao: string;
-  imagemPosicao: ImagemPosicao;
-}
-
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
